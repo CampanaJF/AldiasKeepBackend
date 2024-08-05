19baa57cdedd4ee4111016db0e850db960c8caf0
CampanaJF <jucampana@alumno.unlam.edu.ar>
2024-08-05 09:57:47 -0300

    init

---
+++ src/main/java/com/inkwell/aldias_keep/AldiasKeepApplication.java
@@ -0,0 +1,13 @@
+package com.inkwell.aldias_keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldiasKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldiasKeepApplication.class, args);
+	}
+
+}
